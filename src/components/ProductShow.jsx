b39b41c280a2228b86e23da6eb355bd3196bca8d
Pratishtha Kumari <pratishtha.richu@gmail.com>
2023-07-18 23:09:55 +0530

    Added and animated product name and description

--- src/components/ProductShow.jsx
+++ src/components/ProductShow.jsx
@@ -1,7 +1,55 @@
-import React from 'react'
+import { React, useEffect } from "react";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 export default function ProductShow() {
+  const { ref, inView } = useInView({
+    threshold: 0.3
+  });
+
+  const animation = useAnimation();
+
+  useEffect(() => {
+    console.log("use effect inView value = ", inView);
+    if (inView) {
+      animation.start({
+        x: 0,
+        transition: {
+          type: "spring",
+          duration: 1,
+          bounce: 0.3,
+        },
+      });
+    }
+
+    if (!inView) {
+      animation.start({
+        x: "-100vw",
+        transition: {
+          type: "spring",
+          duration: 1,
+          bounce: 0.3,
+        },
+      });
+    }
+  }, [inView]);
+
   return (
-    <div className='bg-mywhite h-96'>ProductShow</div>
-  )
+    <main ref={ref} className="bg-mywhite h-96 flex">
+      <motion.div
+        animate={animation}
+        className="flex flex-col justify-center text-left w-2/5 mx-9 gap-4 text-myblack"
+      >
+        <h1 className="font-montserrat font-bold text-4xl">WindzStation</h1>
+        <p className="font-montserrat text-xl font-normal">
+          With New Immersive Experience.
+        </p>
+        <p className="font-montserrat text-xl font-light">
+          Unleash Your gaming potential with WindzStation, where gaming dreams
+          take flight with cutting-edge technology and endless adventures.
+        </p>
+      </motion.div>
+      <div>{/* image */}</div>
+    </main>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
     },
-    
+
     extend: {
       colors: {
-        'myblack': '#161617',
-        'lightBlue': '#dff5ff',
-        'lightText': 'rgb(241 245 249)',
-        'hoverText': '#00d1f7',
-        'activeText': '#0d4084',
-        'mywhite': '#f5f5f7',
+        myblack: "#161617",
+        lightBlue: "#dff5ff",
+        lightText: "rgb(241 245 249)",
+        hoverText: "#00d1f7",
+        activeText: "#0d4084",
+        mywhite: "#f5f5f7",
+      },
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
       },
     },
+    plugins: [],
   },
-  plugins: [],
-}
-
+};
