5b7cc46a9997a4544f4d58b8cee392c8ba6b40cd
Pratishtha Kumari <pratishtha.richu@gmail.com>
2023-08-04 23:50:16 +0530

    new version branch

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,6 +1,32 @@
-import React, { useContext, useState, useEffect, createContext } from "react";
-import { auth } from "../firebase";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+
+
+import React, { useContext, useState, useEffect, createContext } from "react";
+import { getAuth, onAuthStateChanged, createUserWithEmailAndPassword } from 'firebase/auth';
+
+const auth = getAuth();
 const AuthContext = createContext();
 
 export function useAuth() {
@@ -11,11 +37,11 @@ export function AuthProvider({ children }) {
   const [currentUser, setCurrentUser] = useState();
 
   function signUp(email, password) {
-    return auth.createUserWithEmailAndPassword(email, password);
+    return createUserWithEmailAndPassword(auth, email, password);
   }
 
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {   //onAuthStateChanged returns a function which when called will unsubscribe from this onAuthStateChanged listener.
+    const unsubscribe = onAuthStateChanged((user) => {   //onAuthStateChanged returns a function which when called will unsubscribe from this onAuthStateChanged listener.
       setCurrentUser(user);
     });
     return unsubscribe;
