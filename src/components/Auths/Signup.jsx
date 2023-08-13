51072beadc31a7442a078a93a437411b5c6ffcac
Pratishtha Kumari <pratishtha.richu@gmail.com>
2023-08-13 16:47:16 +0530

    glassmorphic effect in signup page

--- src/components/Auths/Signup.jsx
+++ src/components/Auths/Signup.jsx
@@ -29,7 +29,7 @@ export default function Signup() {
 
     setLoading(false);
   }
-  
+
   async function handleGoogleSignUp(data) {
     try {
       setError("");
@@ -45,70 +45,74 @@ export default function Signup() {
   }
 
   return (
-    <div className="p-16">
-      <div className="flex flex-col justify-center items-center max-w-lg bg-white m-auto rounded-2xl">
-        <div className="font-bold text-mywhite text-2xl py-10 w-full text-center bg-blueText rounded-t-2xl">
-          <h1>Sign Up</h1>
-        </div>
+    <div className="bg-[url('/src/assets/Images/signup-bg-lg.png')]">
+      <div className="p-16">
+        <div className="flex flex-col justify-center items-center max-w-lg bg-white m-auto rounded-2xl bg-opacity-20 shadow-3xl border-l border-t border-opacity-30 border-neutral-50 backdrop-blur-xs overflow-hidden relative">
+          <div className="w-full">
+            <div className="font-bold text-mywhite text-2xl py-10 text-center bg-blueText">
+              <h1>Sign Up</h1>
+            </div>
+          </div>
 
-        {error && <div className="text-myblack">{error}</div>}
+          {error && <div className="text-mywhite">{error}</div>}
 
-        <form
-          onSubmit={handleSubmit(handleSave)}
-          className="flex flex-col gap-3 pt-10"
-        >
-          <label htmlFor="email" className="text-myblack">
-            Email
-          </label>
-
-          <input
-            type="email"
-            {...register("email", { required: true })}
-            className="rounded-lg px-3 py-1 bg-slate-200"
-          />
-
-          <label htmlFor="password" className="text-myblack">
-            Password
-          </label>
-
-          <input
-            type="password"
-            {...register("password", { required: true })}
-            className="rounded-lg px-3 py-1 bg-slate-200"
-          />
-
-          <label htmlFor="passwordConfirmation" className="text-myblack">
-            Confirm Password
-          </label>
-
-          <input
-            type="password"
-            {...register("passwordConfirmation", { required: true })}
-            className="rounded-lg px-3 py-1 bg-slate-200"
-          />
-
-          <button
-            disabled={loading}
-            type="submit"
-            className="text-mywhite bg-blueText p-2 w-4/5 m-auto mt-10 rounded-2xl"
+          <form
+            onSubmit={handleSubmit(handleSave)}
+            className="flex flex-col gap-3 pt-10"
           >
-            Save
-          </button>
-        </form>
+            <label htmlFor="email" className="text-gray-300">
+              Email
+            </label>
 
-        <p className="py-4">OR</p>
+            <input
+              type="email"
+              {...register("email", { required: true })}
+              className="rounded-lg px-3 py-1 bg-slate-200"
+            />
 
-        <div >
-          <GoogleButton onClick={handleGoogleSignUp}/>
-        </div>
+            <label htmlFor="password" className="text-gray-300">
+              Password
+            </label>
 
-        <div className="font-normal text-myblack py-10">
-          <p>
-            Alread have an account ?{" "}
-            <Link to="/login" className="text-blueText underline">
-              LogIn
-            </Link>
-          </p>
+            <input
+              type="password"
+              {...register("password", { required: true })}
+              className="rounded-lg px-3 py-1 bg-slate-200"
+            />
+
+            <label htmlFor="passwordConfirmation" className="text-gray-300">
+              Confirm Password
+            </label>
+
+            <input
+              type="password"
+              {...register("passwordConfirmation", { required: true })}
+              className="rounded-lg px-3 py-1 bg-slate-200"
+            />
+
+            <button
+              disabled={loading}
+              type="submit"
+              className="text-mywhite bg-blueText p-2 w-4/5 m-auto mt-10 rounded-2xl"
+            >
+              Save
+            </button>
+          </form>
+
+          <p className="py-4 text-gray-300">OR</p>
+
+          <div>
+            <GoogleButton onClick={handleGoogleSignUp} />
+          </div>
+
+          <div className="font-normal text-mywhite py-10">
+            <p>
+              Alread have an account ?{" "}
+              <Link to="/login" className="text-blueText underline">
+                LogIn
+              </Link>
+            </p>
+          </div>
         </div>
       </div>
     </div>
