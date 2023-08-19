b535edcef08efe666493e8ee87ecf12f21df968b
Pratishtha Kumari <pratishtha.richu@gmail.com>
2023-08-19 22:11:49 +0530

    Backdrop animation in signup

--- src/components/Auths/Signup.jsx
+++ src/components/Auths/Signup.jsx
@@ -3,11 +3,13 @@ import { useForm } from "react-hook-form";
 import { useAuth } from "/src/contexts/AuthContext";
 import { Link, useNavigate } from "react-router-dom";
 import { GoogleButton } from "react-google-button";
+import { motion } from "framer-motion";
 
 export default function Signup() {
   const { signUp, googleSignUp } = useAuth();
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
+  const [expanded, setExpanded] = useState(false);    //for backdrop animation
 
   const navigate = useNavigate();
   const { register, handleSubmit } = useForm();
@@ -44,14 +46,42 @@ export default function Signup() {
     setLoading(false);
   }
 
+  const backdropVariants = {
+    expanded: {
+      width: "160%",
+      height: "1700px",
+      borderRadius: "20%",
+    },
+
+    collapsed: {
+      width: "150%",
+      height: "700px",
+      borderRadius: "50%",
+      transform: "rotate(0deg)",
+    }
+  }
+
+  const expandingTransition = {
+    type: "spring",
+    duration: 2.3,
+    stiffness: 30,
+
+  }
+
+  const playExpandingAnimation = () => {
+    setExpanded(true);
+    setTimeout(() => {
+      setExpanded(false);
+    }, expandingTransition.duration * 1000 - 1500);
+  }
+
+
   return (
     <div className="bg-[url('/src/assets/Images/signup-bg-lg.png')]">
       <div className="p-10">
-        <div className="flex flex-col justify-center items-center max-w-lg bg-white m-auto rounded-2xl bg-opacity-20 shadow-3xl border-l border-t border-opacity-30 border-neutral-50 backdrop-blur-xs   overflow-hidden relative">
+        <div className="flex flex-col justify-center items-center max-w-md bg-white m-auto rounded-2xl bg-opacity-20 shadow-3xl border-l border-t border-opacity-30 border-neutral-50 backdrop-blur-xs overflow-hidden relative">
           <div className="w-full h-52 flex">
-            <div className="flex flex-col justify-end">
-              <div className="w-[150%] h-[700px] absolute bg-blueText bg-gradient-to-l from-[rgba(25,91,179,1)-95%] to-[rgba(13,64,132,1)] rounded-[50%] flex flex-col -left-44"></div>
-            </div>
+              <motion.div initial={false} animate={expanded ? "expanded" : "collapsed"} variants={backdropVariants} transition={expandingTransition} className="w-[150%] h-[700px] absolute bg-blueText bg-gradient-to-l from-[rgba(25,91,179,1)-95%] to-[rgba(13,64,132,1)] rounded-[50%] flex flex-col -left-44 -top-[60%]"></motion.div>
             <div className="font-bold text-mywhite text-3xl md:text-4xl z-10 pl-28 pt-20">
               <h1>Sign Up</h1>
             </div>
@@ -116,6 +146,10 @@ export default function Signup() {
               </Link>
             </p>
           </div>
+          
+            <p onClick={playExpandingAnimation}>
+              Click Me
+            </p>
         </div>
       </div>
     </div>
