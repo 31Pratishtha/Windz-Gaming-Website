038640b5d7b5e6d05d50707e05393d30d87fde31
Pratishtha Kumari <pratishtha.richu@gmail.com>
2023-09-24 12:31:06 +0530

    src path change test

--- src/components/Homepage/ProductShowPs5.jsx
+++ src/components/Homepage/ProductShowPs5.jsx
@@ -69,7 +69,7 @@ export default function ProductShowPs5() {
           }}
           animate={ps5Amimaton}
           initial={{ x: "100vw" }}
-          src="/src/assets/Images/ps5.png"
+          src="./src/assets/Images/ps5.png"
           alt="ps5"
         />
       </div>
