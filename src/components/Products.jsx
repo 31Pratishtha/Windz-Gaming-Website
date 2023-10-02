c7050d8ccdf3f0279d5b12dc7e95aa34b46a140a
Pratishtha Kumari <pratishtha.richu@gmail.com>
2023-10-02 15:39:16 +0530

    Add to cart button

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,17 +1,27 @@
 import React, { useEffect, useState} from "react"; // Import useEffect
 import { useProduct } from "/src/contexts/ProductContext";
+import {useAuth} from "/src/contexts/AuthContext"
+import { useNavigate } from "react-router-dom";
+
 
 export default function Products() {
+  const { currentUser } = useAuth();
   const { products } = useProduct();
+  const navigate = useNavigate();
 
   const handleAddToCart = () => {
-    
+    if(currentUser) {
+      alert("Added to cart");
+    }
+    else{
+      navigate("/authentication", {replace: true})
+    }
   }
 
   return <div className="text-lightText">
     <div className="text-myblack flex flex-col ">
       {products.map((product) => (
-        <div key={product.id} className="flex justify-between bg-mywhite">
+        <div key={product.id} className="flex justify-between items-center bg-mywhite">
           <h2>{product.Name}</h2>
           <p>Price: ${product.Price}</p>
           <img src={product.Image} width={200} height={200} alt={product.Name} />
