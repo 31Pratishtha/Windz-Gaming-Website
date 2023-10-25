8a8d3f4a9e1004ecfe782e2587d3d92474312057
Pratishtha Kumari <pratishtha.richu@gmail.com>
2023-10-25 23:29:22 +0530

    products UI

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { useAuth } from '/src/contexts/AuthContext'
+
+
 
 export default function Cart() {
   return (
-    <div className='text-lightText'>Cart</div>
+    <div className='text-myblack bg-mywhite'>
+      
+    </div>
   )
 }
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,60 +1,74 @@
 import React, { useEffect, useState } from "react"; // Import useEffect
 import { useProduct } from "/src/contexts/ProductContext";
-import {useAuth} from "/src/contexts/AuthContext"
+import { useAuth } from "/src/contexts/AuthContext";
 import { useNavigate } from "react-router-dom";
 import { db } from "../firebase";
-import {   collection, doc, getDoc, setDoc } from 'firebase/firestore';
-
+import { collection, doc, getDoc, setDoc } from "firebase/firestore";
 
 export default function Products() {
   const { currentUser } = useAuth();
   const { products } = useProduct();
   const navigate = useNavigate();
 
-  const cartCollectionRef = collection(db, "Users", currentUser.uid, "Cart");
-  
-  const handleAddToCart = async(prod) => {
-    
-    if(currentUser) {
-      // console.log("prodId: ", prod.id); //abcd
-      const cartItemDocId = prod.id;
-      const cartItemsDocRef = doc(cartCollectionRef, cartItemDocId)
-      // console.log("prodName: ", prod.Name);
-      // console.log("prodprice: ", prod.Price);
-      try{
-        const cartItemSnapshot = await getDoc(cartItemsDocRef);
-        console.log("cartItemSnapshot: ", cartItemSnapshot.id); //abcd
-
-        if(!cartItemSnapshot.exists()){
-          // console.log("exist?: ", cartItemSnapshot.exists());
-          await setDoc(cartItemsDocRef, {name: prod.Name, price: prod.Price, qty: 1})
-        }
-        else{
-          const cartItemData = cartItemSnapshot.data();
-          const updatedQty = (cartItemData.qty || 0) + 1;
-          await setDoc(cartItemsDocRef, {...cartItemData, qty: updatedQty});
-        }
-
-      }catch(error){
-        console.log(error);
-      } 
-      // console.log("CartItem: ", cartItemsDocRef.id); //abcd
-    }
-    else{
-      navigate("/authentication", {replace: true})
-    }
+  let cartCollectionRef = null;
+  if (currentUser) {
+    cartCollectionRef = collection(db, "Users", currentUser.uid, "Cart");
   }
 
-  return <div className="text-lightText">
-    <div className="text-myblack flex flex-col ">
-      {products.map((product) => (
-        <div key={product.id} className="flex justify-between items-center bg-mywhite">
-          <h2>{product.Name}</h2>
-          <p>Price: ${product.Price}</p>
-          <img src={product.Image} width={200} height={200} alt={product.Name} />
-          <button onClick={() => handleAddToCart(product)}>Add to cart</button>
-        </div>
-      ))}
+  const handleAddToCart = async (prod) => {
+    if (currentUser) {
+      const cartItemDocId = prod.id;
+      const cartItemsDocRef = doc(cartCollectionRef, cartItemDocId);
+      try {
+        const cartItemSnapshot = await getDoc(cartItemsDocRef);
+        console.log("cartItemSnapshot: ", cartItemSnapshot.id);
+
+        if (!cartItemSnapshot.exists()) {
+          await setDoc(cartItemsDocRef, {
+            name: prod.Name,
+            price: prod.Price,
+            qty: 1,
+          });
+        } else {
+          const cartItemData = cartItemSnapshot.data();
+          const updatedQty = (cartItemData.qty || 0) + 1;
+          await setDoc(cartItemsDocRef, { ...cartItemData, qty: updatedQty });
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      navigate("/authentication", { replace: true });
+    }
+  };
+
+  return (
+    <div className="text-myblack flex flex-col bg-mywhite ">
+      <div className="m-4">
+        {products.map((product) => (
+          <div
+            key={product.id}
+            className="grid grid-cols-4 gap-2 my-4 items-center bg-slate-200 px-8 py-2 rounded-lg"
+          >
+            <img
+              src={product.Image}
+              width={150}
+              height={150}
+              alt={product.Name}
+            />
+            <h2>{product.Name}</h2>
+            <p className="flex justify-center font-semibold">${product.Price}</p>
+            <div className="flex justify-center">
+              <button
+                onClick={() => handleAddToCart(product)}
+                className="bg-slate-800 text-mywhite py-2 w-40 rounded-lg"
+              >
+                Add to cart
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  </div>;
+  );
 }
