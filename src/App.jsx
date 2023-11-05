35d2747d8b825952bfdfb40716f7b795ba54db1b
Pratishtha Kumari <pratishtha.richu@gmail.com>
2023-11-05 22:12:04 +0530

    cart items setup

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,20 @@ import Footer from "./components/Footer";
 import { AuthProvider } from "./contexts/AuthContext";
 import { ProductProvider } from "./contexts/ProductContext";
 import { UserProvider } from "./contexts/UserContext";
+import { CartItemsProvider } from "./contexts/CartItemsContext";
+
 export default function App() {
   return (
     <AuthProvider>
       <ProductProvider>
         <UserProvider>
-          <div className="bg-myblack min-h-screen">
-            <Navbar />
-            <Main />
-            <Footer />
-          </div>
+          <CartItemsProvider>
+            <div className="bg-myblack min-h-screen">
+              <Navbar />
+              <Main />
+              <Footer />
+            </div>
+          </CartItemsProvider>
         </UserProvider>
       </ProductProvider>
     </AuthProvider>
